b8fd174684e2465191aff57435e7f63dc1e7645e
Ramos <72244255+Gu-Ramos@users.noreply.github.com>
2024-09-15 01:22:05 -0300

    to_dict() do author.py agora também converte os livros pra dicionário

--- author.py
+++ author.py
@@ -6,9 +6,12 @@ class Author:
         self.nationality = nationality
 
     def to_dict(self):
+        books = {}
+        for book_id, book in self.books.items():
+            books[book_id] = book.to_dict()
         return {
             'name': self.name,
-            'books': self.books,
+            'books': books,
             'birthday': self.birthday,
             'nationality': self.nationality
         }
